f07b78f80d5b62b486e440b8bca1ba3b5c841c31
MurielP <muriel.dev@bbox.fr>
2017-12-12 16:09:47 +0100

    Ajout du chier qui gère la vue d'un billet : view/view_post.php

---
+++ view/view_post.php
@@ -0,0 +1,34 @@
+<article>
+    <header>
+        <h1><?= htmlspecialchars($post['title']) ?></h1>
+         <time><?= $post['creation_date'] ?></time>
+       
+    </header>
+    <p><?= htmlspecialchars($post['content']) ?></p>
+</article>
+<hr />
+<header>
+    <h1>Commentaires : <?= htmlspecialchars($post['title']) ?></h1>
+</header>
+
+<?php foreach ($comments as $key => $comment) : ?> 
+	<article>
+		<p>Commentaire de : <?= htmlspecialchars ($comment['author']) ?> <br/>
+		Le <?= $comment['comment_date'] ?><br/>
+		<?= nl2br(htmlspecialchars($comment['comment'])) ?>
+		</p>
+	</article>
+		<hr/> 
+<?php endforeach; ?>
+
+
+<h2>Laissez-nous votre commentaire</h2>
+	
+<form method="post" action="index.php?action=toComment">
+	<p><label for="author">Auteur du commentaire</label> : <input type="text" name="author" id="author" value=""/></p>
+	<p><label for="comment">Nouveau commentaire</label> : <input type="textarea" name="comment" id="comment" value=""/></p>
+			
+	<input type="hidden" name="id" id="id" value="<?php echo $post['id'];?>"/> 
+
+	<p><input type="submit" value="Postez votre commentaire" /></p>
+</form>
