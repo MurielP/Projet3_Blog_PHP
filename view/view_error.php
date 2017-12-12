9a0bd2a0f408027fbb6a4a891273281256de1058
MurielP <muriel.dev@bbox.fr>
2017-12-12 16:12:40 +0100

    Ajout du fichier qui génère la vue des erreurs : view/view_error.php

---
+++ view/view_error.php
@@ -0,0 +1,6 @@
+<!-- conserver l'affichage du template des vues même en cas d'erreur, on reprend le code vueAccueil-->
+<title><?php  $this->title = 'Mon Blog en POO'; ?></title>
+
+	<p>Une erreur est survenue : <?= $errorMessage ?></p>
+
+
