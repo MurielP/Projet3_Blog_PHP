efbd5aee93b3753d2dead503ae3e7310bb2c7e51
MurielP <muriel.dev@bbox.fr>
2017-12-12 16:07:29 +0100

    Ajout du fichier qui gère la vue de la page d'accueil : view/view_home/php

---
+++ view/view_home.php
@@ -0,0 +1,15 @@
+<?php foreach ($posts as $key => $post) : ?> 
+	
+	<article>
+		<header>
+			<a href="index.php?action=post&id=<?php echo $post['id']; ?>">
+			<h1><?= htmlspecialchars($post['title']) ?></h1></a>
+			De : <?= htmlspecialchars($post['author']) ?><br />
+			Le <time><?= $post['creation_date'] ?></time>
+		</header>
+			<p><?=  htmlspecialchars($post['content']) ?></p>
+	</article>
+		<hr/> 
+<?php endforeach; ?>
+
+	
