7d44392856f76a0607a90e1ca349ef056498d27f
MurielP <muriel.dev@bbox.fr>
2017-12-18 18:05:30 +0100

    Ajout du lien de retour vers l'index

--- view/view_error.php
+++ view/view_error.php
@@ -1,6 +1,8 @@
+
 <!-- conserver l'affichage du template des vues même en cas d'erreur, on reprend le code vueAccueil-->
-<title><?php  $this->title = 'Mon Blog en POO'; ?></title>
+<title><?php  $this->title = 'Page d\'erreur'; ?></title>
 
 	<p>Une erreur est survenue : <?= $errorMessage ?></p>
+	<p><a href="index.php">Revenir à la page d'accueil</a></p>
 
 
