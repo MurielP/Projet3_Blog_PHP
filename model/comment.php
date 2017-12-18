3532643803437ab1f03b7e550b318553a97f2296
MurielP <muriel.dev@bbox.fr>
2017-12-18 17:59:20 +0100

    Modifications des commentaires

--- model/comment.php
+++ model/comment.php
@@ -4,18 +4,27 @@ require_once 'connection.php';
 class Comment extends Database 
 {
 
-	// Renvoie la liste des commentaires associés à 1 billet
+/**
+ * getComments renvoie la liste des commentaires associés à 1 billet
+ * @param  $postId soit l'Id du billet sélectionné
+ * @return tous les commentaires liés à l'id du billet choisi     
+ */
 	public function getComments($postId) 
 	{
-			$sql = ('SELECT id, post_id, author, comment, DATE_FORMAT(comment_date,\'%d %m %Y à %Hh%imin%ss\')  AS comment_date FROM comments WHERE post_id = ? ORDER BY comment_date');
-			$comments = $this->executequery($sql, array($postId));
-			return $comments;
-		}
+		$sql = ('SELECT id, post_id, author, comment, DATE_FORMAT(comment_date,\'%d %m %Y à %Hh%imin%ss\')  AS comment_date FROM comments WHERE post_id = ? ORDER BY comment_date');
+		$comments = $this->executequery($sql, array($postId));
+		return $comments;
+	}
 
-	// // traiter et insérer les données du formulaire dans la bdd
+/**
+ * addComment traiter et insérer les données du formulaire dans la bdd
+ * @param [string] $author  [auteur du commentaire]
+ * @param [string] $comment [commentaire]
+ * @param [int] $post_id [id du billet choisi]
+ */
 	public function addComment($author, $comment, $post_id) 
 	{
-			$sql = ('INSERT INTO comments (author, comment, post_id, comment_date) VALUES (?,?,?,NOW())');
-			$this->executeQuery($sql, array($author, $comment, $post_id));
-		}
+		$sql = ('INSERT INTO comments (author, comment, post_id, comment_date) VALUES (?,?,?,NOW())');
+		$this->executeQuery($sql, array($author, $comment, $post_id));
+	}
 }
